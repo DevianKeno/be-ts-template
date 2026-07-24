8162893a4419c6757a63dc611468072ba13812e6
deviank <70948471+DevianKeno@users.noreply.github.com>
2026-07-24 21:40:48 +0800

    fix: update main fn

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,11 +1,5 @@
 import { world, system } from "@minecraft/server";
 
-function mainTick() {
-	if (system.currentTick % 100 === 0) {
-		world.sendMessage("Hello starter! Tick: " + system.currentTick);
-	}
+function main() {}
 
-	system.run(mainTick);
-}
-
-system.run(mainTick);
+system.run(main);
